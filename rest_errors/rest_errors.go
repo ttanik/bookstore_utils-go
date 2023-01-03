7ef2a78ae034a096ce3f6fa2c44f8a7bdf86b9f6
Tulio Tanikawa <tulio_t@hotmail.com>
2023-01-03 07:31:31 -0300

    Refactor errors

--- rest_errors/rest_errors.go
+++ rest_errors/rest_errors.go
@@ -1,29 +1,64 @@
 package rest_errors
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
 )
 
-type RestErr struct {
+type RestErr interface {
+	GetMessage() string
+	GetStatus() int
+	GetError() string
+	GetCauses() []interface{}
+}
+type restErr struct {
 	Message string        `json:"message"`
 	Status  int           `json:"status"`
 	Error   string        `json:"error"`
 	Causes  []interface{} `json:"causes"`
 }
 
-func NewBadRequestError(message string) *RestErr {
-
-	return &RestErr{Message: message, Status: http.StatusBadRequest, Error: "bad_request"}
+func NewRestError(message string, status int, err string, causes []interface{}) RestErr {
+	return restErr{Message: message, Status: status, Error: err, Causes: causes}
+}
+func NewError(msg string) error {
+	return errors.New(msg)
+}
+func NewBadRequestError(message string) RestErr {
+	return restErr{Message: message, Status: http.StatusBadRequest, Error: "bad_request"}
 }
 
-func NewNotFoundError(message string) *RestErr {
-	return &RestErr{Message: message, Status: http.StatusNotFound, Error: "not_found"}
+func NewNotFoundError(message string) RestErr {
+	return restErr{Message: message, Status: http.StatusNotFound, Error: "not_found"}
 }
 
-func NewInternalServerError(message string, err error) *RestErr {
-	result := &RestErr{Message: message, Status: http.StatusInternalServerError, Error: "internal_server_error"}
+func NewInternalServerError(message string, err error) RestErr {
+	result := restErr{Message: message, Status: http.StatusInternalServerError, Error: "internal_server_error"}
 	if err != nil {
 		result.Causes = append(result.Causes, err.Error())
 	}
 	return result
+}
+func (e restErr) GetError() string {
+	return fmt.Sprintf("message: %s - status: %d - error: %s - causes: [%v]", e.Message, e.Status, e.Error, e.Causes)
+}
+func (e restErr) GetMessage() string {
+	return e.Message
+}
+func (e restErr) GetStatus() int {
+	return e.Status
+}
+
+func (e restErr) GetCauses() []interface{} {
+	return e.Causes
+}
+
+func NewRestErrorFromBytes(bytes []byte) (RestErr, error) {
+	var apiErr restErr
+	if err := json.Unmarshal(bytes, &apiErr); err != nil {
+		return nil, errors.New("invalid json")
+	}
+	return apiErr, nil
 }
--- rest_errors/rest_errors_test.go
+++ rest_errors/rest_errors_test.go
@@ -10,26 +10,38 @@ import (
 
 func TestShouldGiveNewInternalServerError(t *testing.T) {
 	err := NewInternalServerError("teste", errors.New("database error"))
-	assert.Equal(t, "teste", err.Message)
-	assert.Equal(t, http.StatusInternalServerError, err.Status)
+	assert.Equal(t, "teste", err.GetMessage())
+	assert.Equal(t, http.StatusInternalServerError, err.GetStatus())
 	assert.NotNil(t, err)
-	assert.Equal(t, "internal_server_error", err.Error)
-	assert.Equal(t, "database error", err.Causes[0])
-	assert.Equal(t, 1, len(err.Causes))
+	assert.Equal(t, "message: teste - status: 500 - error: internal_server_error - causes: [[database error]]", err.GetError())
+	assert.Equal(t, "database error", err.GetCauses()[0])
+	assert.Equal(t, 1, len(err.GetCauses()))
 }
 
 func TestShouldGiveNewNotFoundError(t *testing.T) {
 	err := NewNotFoundError("teste")
-	assert.Equal(t, "teste", err.Message)
-	assert.Equal(t, http.StatusNotFound, err.Status)
+	assert.Equal(t, "teste", err.GetMessage())
+	assert.Equal(t, http.StatusNotFound, err.GetStatus())
 	assert.NotNil(t, err)
-	assert.Equal(t, "not_found", err.Error)
+	assert.Equal(t, "message: teste - status: 404 - error: not_found - causes: [[]]", err.GetError())
 }
 
 func TestShouldGiveBadRequestError(t *testing.T) {
 	err := NewBadRequestError("teste")
-	assert.Equal(t, "teste", err.Message)
-	assert.Equal(t, http.StatusBadRequest, err.Status)
+	assert.Equal(t, "teste", err.GetMessage())
+	assert.Equal(t, http.StatusBadRequest, err.GetStatus())
 	assert.NotNil(t, err)
-	assert.Equal(t, "bad_request", err.Error)
+	assert.Equal(t, "message: teste - status: 400 - error: bad_request - causes: [[]]", err.GetError())
+}
+
+func TestGetters(t *testing.T) {
+	err := NewBadRequestError("bad request error")
+	causes := err.GetCauses()
+	message := err.GetMessage()
+	status := err.GetStatus()
+	error := err.GetError()
+	assert.Equal(t, []interface{}([]interface{}(nil)), causes)
+	assert.Equal(t, "bad request error", message)
+	assert.Equal(t, http.StatusBadRequest, status)
+	assert.Equal(t, "message: bad request error - status: 400 - error: bad_request - causes: [[]]", error)
 }
